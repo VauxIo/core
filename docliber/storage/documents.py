2073c927697c3b2b87a75f41f611a375e016fe96
Ross Delinger <rossdylan@csh.rit.edu>
2014-09-27 18:39:06 -0400

    MEGACOMMIT, stripped out all the leveldb and switched to rethinkdb so we can actually have multiple processes accessing the data

--- docliber/storage/documents.py
+++ docliber/storage/documents.py
@@ -6,10 +6,18 @@ import os.path
 from docliber.storage.metadata import MetaEngine
 from hashlib import sha1
 import itertools
+import datetime
+import pytz
 
 
 def take(n, i):
     return list(itertools.islice(i, n))
+
+
+class DocumentExistsError(Exception):
+    def __init__(self, message, docid):
+        Exception.__init__(self, message)
+        self.docid = docid
 
 
 class DocEngine():
@@ -20,11 +28,11 @@ class DocEngine():
             aa/
                 <hash of document>
     """
-    def __init__(self, root_path):
+    def __init__(self, root_path, hostname, port, db):
         self.root_path = root_path
         self.doc_path = os.path.join(self.root_path, "documents")
         self._create_dirs()
-        self.index = MetaEngine(self.doc_path, dbname='index')
+        self.index = MetaEngine(hostname, port, db, 'documents')
 
     def _create_dirs(self):
         """
@@ -33,92 +41,69 @@ class DocEngine():
         if not os.path.exists(self.doc_path):
             os.mkdir(self.doc_path)
 
-    def _add_documents_thread(self, paths):
-        index_kvs = []
-        greenlets = []
-
-        def injest(path):
-            name = os.path.basename(path)
-            hasher = sha1()
-            print("injesting {0}".format(path))
-            with open(path) as fd:
-                while True:
-                    data = fd.read(hasher.block_size)
-                    if data == '':
-                        break
-                    hasher.update(data)
-            file_hash = hasher.hexdigest()
-            subdir = os.path.join(self.doc_path, file_hash[0:2])
-            if not os.path.exists(subdir):
-                os.mkdir(subdir)
-            os.rename(path, os.path.join(subdir, file_hash))
-            index_kvs.append(("{0}_hash".format(name), file_hash))
-
-        for path in paths:
-            greenlets.append(gevent.spawn(injest, path))
-        gevent.joinall(greenlets)
-        self.index.batch_put_pickle(index_kvs)
-
-    def add_documents(self, paths):
+    def add_document(self, path):
         """
-        Add a list of documents to the store
+        object format is: {
+            "name": <str>,
+            "size": <int>,
+            "upload_time": <datetime>
+            "path": <str> }
         """
-        greenlets = []
-        while True:
-            batch = take(20, paths)
-            print batch
-            greenlets.append(gevent.spawn(self._add_documents_thread, batch))
-            if len(batch) < 20:
-                break
-        gevent.joinall(greenlets)
-        print("Added {0} documents".format(len(paths)))
+        index_objects = []
 
-    def get_hash(self, doc_name):
-        key = "{0}_hash".format(doc_name)
-        if self.index.has_key(key):
-            return self.index.load_pickle(key)
-        return None
+        name = os.path.basename(path)
+        hasher = sha1()
+        print("injesting {0}".format(path))
+        size = os.stat(path).st_size
+        upload_time = datetime.datetime.now(pytz.utc)
+        with open(path) as fd:
+            while True:
+                data = fd.read(hasher.block_size)
+                if data == '':
+                    break
+                hasher.update(data)
+        file_hash = hasher.hexdigest()
+        subdir = os.path.join(self.doc_path, file_hash[0:2])
+        if not os.path.exists(subdir):
+            os.mkdir(subdir)
+        final_path = os.path.join(subdir, file_hash)
+        if self.index.exists({'path': final_path}):
+            original = self.index.load({'path': final_path})[0]
+            raise DocumentExistsError("This document already exists", original['id'])
+        os.rename(path, final_path)
+        index_objects.append({
+            'name': name,
+            'size': size,
+            'upload_time': upload_time,
+            'path': final_path})
+        self.index.put(index_objects)
 
-    def get_document_path(self, doc_name):
+    def get_document_path(self, docid):
         """
         Return the path to a document if we have it. This function
         expects the full name of a document"
         """
-        fhash = self.get_hash(doc_name)
-        if fhash is not None:
-            subdir = fhash[0:2]
-            return os.path.join(self.doc_path, subdir, fhash)
+        results = self.index.load({"id": docid})
+        if len(results) > 0:
+            return results[0]
         return None
 
     def search_documents(self, search_str):
         """
         Search our index by file name
         """
-        for key, _ in self.index.get_all(search_str):
-            name = key.split('_')[0]
-            if search_str in name:
-                yield name
+        for doc in self.index.search(lambda doc: search_str in doc['name']):
+            yield doc
 
-    def _remove_thread(self, name):
-        """
-        function to be run in a greenlet in order to remove documents
-        """
-        path = self.find_document(name)
-        os.remove(path)
-        self.index.delete("{0}_hash".format(name))
-
-    def remove_documents(self, names):
+    def remove_document(self, docid):
         """
         Spin up greenlets to remove a list of documents
         """
-        greenlets = []
-        for name in names:
-            greenlets.append(gevent.spawn(self._remove_thread, name))
-        gevent.joinall(greenlets)
+        self.index.delete({'id': docid})
 
     def list_documents(self):
         """
         Return a list of all documents
         """
-        for key, val in self.index.get_all("_hash"):
-            yield key.split("_")[0]
+        for doc in self.index.get_all():
+            yield doc
