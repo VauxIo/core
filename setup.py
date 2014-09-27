6dbf3c97736f78261e4e06e452e72b789b1f52fe
Ross Delinger <rossdylan@csh.rit.edu>
2014-09-27 09:45:10 -0400

    Initial project setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+requires = [
+    'leveldb',
+    'falcon',
+]
+
+setup(
+    name='docliber',
+    version='0.1.0',
+    description='Document Liberation',
+    packages=find_packages(),
+    install_requires=requires
+)
