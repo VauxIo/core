bdde09930676caacfc3d11dc648460fa4f39be83
Ross Delinger <rossdylan@csh.rit.edu>
2014-09-27 16:47:05 -0400

    Fixed pep8 issues in the api code

--- docliber/api/__init__.py
+++ docliber/api/__init__.py
@@ -2,60 +2,50 @@ from flask import Flask, abort, request
 from flask.ext import restful
 from docliber.storage import LibreDB
 
+
 app = Flask(__name__)
 db = LibreDB('../data')
 
+
 class PeerInstance (restful.Resource):
 
-    def get (self, id):
-
+    def get(self, id):
         peers = db.meta.load_pickle('peers')
-
         if id in peers.keys():
-
-        	  peer = peers[id]
-        	  
-        	  peer = {
+            peer = peers[id]
+            peer = {
                 'address': peer['address'],
                 'port': peer['port'],
                 'hostname': peer['hostname'],
                 'last_seen': peer['last_seen'].strftime('%Y-%m-%d %H:%M:%S')
             }
-
-        	  return peer
-
+            return peer
         else:
+            abort(404)
 
-        	  abort(404)
-
-    def delete (self, id):
-
+    def delete(self, id):
         peers = db.meta.load_pickle('peers')
-
         if id in peers.keys():
-
-        	  db.remove_peer(id)
-
+            db.remove_peer(id)
         else:
+            abort(404)
 
-        	  abort(404)
 
-class PeerResource (restful.Resource):
+class PeerResource(restful.Resource):
 
-    def get (self):
-        
+    def get(self):
         peers = [
             {
-            	  'address': peer['address'],
+                'address': peer['address'],
                 'port': peer['port'],
                 'hostname': peer['hostname'],
                 'last_seen': peer['last_seen'].strftime('%Y-%m-%d %H:%M:%S')
             } for peer in db.get_peers()
         ]
 
-        return {'peers': peers } 
+        return {'peers': peers}
 
-    def post (self):
+    def post(self):
 
         address = request.form.get('address')
         port = request.form.get('port')
@@ -65,26 +55,26 @@ class PeerResource (restful.Resource):
         if not address or not port or not hostname or not last_seen:
 
             abort(400)
-        
+
         peer = {
             'address': address,
             'port': port,
             'hostname': hostname,
             'last_seen': datetime.strptime(last_seen, '%Y-%m-%d %H:%M:%S')
         }
-      
+
         db.add_peer(peer)
-      
+
         peers = [
             {
-            	  'address': peer['address'],
+                'address': peer['address'],
                 'port': peer['port'],
                 'hostname': peer['hostname'],
                 'last_seen': peer['last_seen'].strftime('%Y-%m-%d %H:%M:%S')
             } for peer in db.get_peers()
         ]
 
-        return {'peers': peers }        
+        return {'peers': peers}
 
 api = restful.Api(app)
 api.add_resource(PeerResource, '/peers/')
