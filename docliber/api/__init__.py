028679bb3be02551cc22d2288c6a66ed20716648
Mihir Singh <git.service@mihirsingh.com>
2014-09-27 20:03:21 -0400

    Implemented POST for the documents resource

--- docliber/api/__init__.py
+++ docliber/api/__init__.py
@@ -1,7 +1,9 @@
+import os
 from flask import Flask, abort, request
 from flask.ext import restful
 from docliber.storage import LibreDB
 from datetime import datetime
+from werkzeug import secure_filename
 
 app = Flask(__name__)
 db = LibreDB('../data')
@@ -89,6 +91,28 @@ class DocumentResource(restful.Resource):
         ]
 
         return {'documents': documents}
+
+    def post(self):
+
+        file = request.files['file']
+
+        if not file:
+
+            abort(400)
+
+        filename = secure_filename(file.filename)
+
+        if not os.path.exists('/tmp/uploaded/'):
+
+            os.makedirs('/tmp/uploaded/')
+
+        path = os.path.join('/tmp/uploaded/', filename)
+
+        file.save(path)
+
+        db.add_document(path)
+
+        return ''
 
 api = restful.Api(app)
 api.add_resource(PeerResource, '/peers/')
