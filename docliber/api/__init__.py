e2693858b70bb856d464094eaaa376fb378cb9db
Mihir Singh <git.service@mihirsingh.com>
2014-09-27 22:06:35 -0400

    Added CORS support to the API endpoints

--- docliber/api/__init__.py
+++ docliber/api/__init__.py
@@ -4,6 +4,7 @@ from flask.ext import restful
 from docliber.storage import LibreDB
 from datetime import datetime
 from werkzeug import secure_filename
+from cors import crossdomain
 
 app = Flask(__name__)
 database = LibreDB('../data', 'localhost', 28015, 'docliber')
@@ -12,6 +13,9 @@ from peer import PeerResource, PeerInstance
 from document import DocumentResource, DocumentInstance
 
 api = restful.Api(app)
+
+api.decorators=[crossdomain(origin='*')]
+
 api.add_resource(PeerResource, '/peers/')
 api.add_resource(PeerInstance, '/peers/<string:id>/')
 api.add_resource(DocumentResource, '/documents/')
